e7ed530b006e6ca4ae5fb73a5ddf4d39cc9fb57a
Derrick J Chie <djchie.dev@gmail.com>
2015-07-26 16:23:56 +0200

    Updated the getElementsByClassName function to use document.body, element.childNodes, and element.classList properties

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -9,18 +9,19 @@ var getElementsByClassName = function(className){
   var classElements = [];
   // Define a function for testing className
   var checkClass = function(element, className) {
-  	var $element = $(element);
-  	console.log($element);
-  	  // Check if current element has class, if so add to array
-      if ($element.hasClass(className)) {
-      	classElements.push($element[0]/*.localName + "." + className*/);
+  	// Check if the current element is text, if so, ignore
+	if (element.nodeName !== "#text") {
+	  // Check if current element has class, if so add to array
+	  if (element.classList.contains(className)) {
+  	    classElements.push(element/*.localName + "." + className*/);
+  	  }
+  	  // If current element has nested, call getElementsByClassName on those nested elements
+	  for (var i = 0; i < element.childNodes.length; i++) {
+        checkClass(element.childNodes[i], className);
       }
-      // If current element has nested, call getElementsByClassName on those nested elements
-      for (var i = 0; i < $element.children().length; i++) {
-      	checkClass($element.children()[i], className);
-      }
+    }
   }
   // Initiate recursive call
-  checkClass(document, className);
+  checkClass(document.body, className);
   return classElements;
 };
