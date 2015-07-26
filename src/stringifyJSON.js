6534fda476c5dd7f82c3a3e5740b82dbae2ee504
Derrick J Chie <djchie.dev@gmail.com>
2015-07-26 12:26:29 +0200

    Implemented the stringifyJSON function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,42 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // If null, return stringed version of null
+  if (obj === null) {
+  	return String(obj);
+  }
+  // If undefined or is function, just return
+  if (obj === undefined || obj.constructor === Function) {
+  	return;
+  }
+  // If number, boolean, or string, return with specific format
+  if (obj.constructor !== Object && obj.constructor !== Array) {
+  	if (obj.constructor === String) {
+  	  return "\"" + obj + "\"";
+    }
+    if (obj.constructor === Number) {
+      return obj.toString();
+    }
+    return String(obj);
+  }
+  // If array, make recursive calls to each member
+  if (obj.constructor === Array) {
+  	var stringArray = new Array();
+  	for (var i = 0; i < obj.length; i++) {
+  		stringArray.push(stringifyJSON(obj[i]));
+  	}
+  	return "[" + stringArray.join(",") + "]";
+  }
+  // If object, make recursive calls to each key/value pair
+  if (obj.constructor === Object) {
+  	var stringArray = new Array();
+  	for (var key in obj) {
+  	  console.log("The key is: " + key);
+  	  console.log("The object is: " + obj[key]);
+  	  if (obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+  	  	stringArray.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+  	  }
+  	}
+  	return "{" + stringArray.join(",") + "}";
+  }
 };
