f87accb2815bf1d6db05ef96e0146d06a0b38605
Derrick J Chie <djchie.dev@gmail.com>
2015-07-26 14:24:08 +0200

    Implemented the getElementsByClassName function

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  var classElements = [];
+  // Define a function for testing className
+  var checkClass = function(element, className) {
+  	var $element = $(element);
+  	console.log($element);
+  	  // Check if current element has class, if so add to array
+      if ($element.hasClass(className)) {
+      	classElements.push($element[0]/*.localName + "." + className*/);
+      }
+      // If current element has nested, call getElementsByClassName on those nested elements
+      for (var i = 0; i < $element.children().length; i++) {
+      	checkClass($element.children()[i], className);
+      }
+  }
+  // Initiate recursive call
+  checkClass(document, className);
+  return classElements;
 };
